dbe8ad9cf5d3751b2bb5f684b0ae82e942e8aa54
max-talanov <max.talanov@gmail.com>
2026-02-06 13:10:34 +0100

    fixed long-run

--- run.sh
+++ run.sh
@@ -29,6 +29,5 @@ srun --cpu-bind=cores --distribution=block:block \
     --sim-ms 10000 \
     --dt-ms 10 \
     --threads $SLURM_CPUS_PER_TASK \
-    --long-run \
     --nest-verbosity M_ERROR \
     --max-weight-conns 2000
