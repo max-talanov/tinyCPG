e32504a84103e599b71b62f13ab1d22257c5674f
max-talanov <max.talanov@gmail.com>
2026-02-18 11:05:15 +0100

    10 s 20 ms save

--- run.sh
+++ run.sh
@@ -26,10 +26,10 @@ PY
 srun --cpu-bind=cores --distribution=block:block \
   python3 -u cpg_2legs_fast.py \
     --out cpg_${SLURM_JOB_ID}.h5 \
-    --sim-ms 120000 \
+    --sim-ms 10000 \
     --dt-ms 10 \
     --threads $SLURM_CPUS_PER_TASK \
     --nest-verbosity M_ERROR \
     --max-weight-conns 2000 \
     --save-weights snapshots\
-    --long-run
+    --simulate-chunk-ms 20 --rate-update-ms 20 --resolution-ms 0.2
